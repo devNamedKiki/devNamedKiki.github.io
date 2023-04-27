8d3e9264d3feb303c8fb435e2867b56dc8ae53fc
devNamedKiki <kiki.dev@proton.me>
2023-04-27 06:07:57 -0700

    start of website

---
+++ Oa/script.js
@@ -0,0 +1,7 @@
+function clicked(element) {
+    var allCells = document.getElementsByTagName("td");
+    for (var i = 0; i < allCells.length; i++) {
+      allCells[i].classList.remove("clicked");
+    }
+    element.parentNode.classList.add("clicked");
+  }
