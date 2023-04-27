6d83f15545b8adac7de4e643d03d445fce2c8d49
devNamedKiki <kiki.dev@proton.me>
2023-04-27 16:52:07 -0700

    About me

--- About/script.js
+++ About/script.js
@@ -1,9 +1,22 @@
-// Smooth scrolling
-const scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 800
+// Get the tab links and tab content
+const tabLinks = document.querySelectorAll(".tabs li");
+const tabContent = document.querySelectorAll(".tab-content");
+
+// Set the default tab
+tabLinks[0].classList.add("active");
+tabContent[0].classList.add("active");
+
+// Add event listeners to the tab links
+for (let i = 0; i < tabLinks.length; i++) {
+  tabLinks[i].addEventListener("click", function() {
+    // Remove the active class from all tab links and tab content
+    for (let j = 0; j < tabLinks.length; j++) {
+      tabLinks[j].classList.remove("active");
+      tabContent[j].classList.remove("active");
+    }
+    // Add the active class to the clicked tab link and corresponding tab content
+    this.classList.add("active");
+    const tab = this.getAttribute("data-tab");
+    document.querySelector(`#${tab}`).classList.add("active");
   });
-  
-  // Toggle mobile menu
-  const navToggle = document.querySelector('.nav-toggle');
-  const navLinks = document.querySelectorAll
-  
+}
