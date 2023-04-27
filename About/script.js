00d7b73a4604de39ba0662d1f82abbe0c8c3bad2
devNamedKiki <kiki.dev@proton.me>
2023-04-27 06:19:55 -0700

    about

---
+++ About/script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling
+const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 800
+  });
+  
+  // Toggle mobile menu
+  const navToggle = document.querySelector('.nav-toggle');
+  const navLinks = document.querySelectorAll
+  
