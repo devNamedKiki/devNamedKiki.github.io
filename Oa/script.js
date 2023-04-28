0410e3c1591de5a999e3a31009474b2fc07cdda8
devNamedKiki <kiki.dev@proton.me>
2023-04-28 08:10:06 -0700

    rough draft

--- Oa/script.js
+++ Oa/script.js
@@ -1,8 +1,16 @@
-function clicked(element) {
-    var allCells = document.getElementsByTagName("td");
-    for (var i = 0; i < allCells.length; i++) {
-      allCells[i].classList.remove("clicked");
-    }
-    element.parentNode.classList.add("clicked");
-  }
+const cols2and3 = document.querySelectorAll('td:nth-child(2), td:nth-child(3)');
+
+cols2and3.forEach((col) => {
+  let timeoutId;
   
+  col.addEventListener('mouseenter', () => {
+    timeoutId = setTimeout(() => {
+      col.classList.add('show');
+    }, 1500);
+  });
+  
+  col.addEventListener('mouseleave', () => {
+    clearTimeout(timeoutId);
+    col.classList.remove('show');
+  });
+});
